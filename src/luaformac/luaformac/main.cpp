224b5d57073e14bd2fa2aff2fda14b626a0b2909
Chen Bingfeng <iichenbf@gmail.com>
2015-04-22 22:22:25 +0800

    add xcode project file

---
+++ src/luaformac/luaformac/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  luaformac
+//
+//  Created by chenbingfeng on 15/4/22.
+//  Copyright (c) 2015年 chenbingfeng. All rights reserved.
+//
+
+#include <iostream>
+
+/*
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}*/
